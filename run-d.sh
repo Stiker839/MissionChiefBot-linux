190dec51185cbf3c2f61c12f63d9ec297cadaa0c
Jack Bayliss <jack@jackbayliss.com>
2020-04-17 17:56:27 +0100

    Addition of DockerFile
    Addition of DockerFile for docker.

--- run-d.sh
+++ run-d.sh
@@ -2,4 +2,4 @@ echo - MISSION CHIEF BOT 16/02/2020-
 echo - Creative Commons Attribution-NonCommercial 4.0 International License -
 echo - Copyright 2020 Jack Bayliss / Joshua Latham license available - https://github.com/codesidian/Python-MissionChiefBot/blob/master/LICENSE.md -
 cd botfiles
-python3 missionchief_bot.py
+python3 missionchief_bot.py & python3 despatcher.py
